238ea17fc12928be1b0d3a72fad51af682f6163c
kailan <kailan.paul@pg.canterbury.ac.nz>
2023-09-25 20:08:11 +1300

    update repo, usb not working with CAN

---
+++ master_node/Core/Inc/main.h
@@ -0,0 +1,20 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#include "stm32f4xx_hal.h"
+
+extern CAN_HandleTypeDef hcan1;
+extern UART_HandleTypeDef huart4;
+extern I2C_HandleTypeDef hi2c2;
+
+void Error_Handler(void);
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* __MAIN_H */
